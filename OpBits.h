c1009623cb73602bdd7c44753f0decdd3add490d
joshuamsje <49621706+joshuamsje@users.noreply.github.com>
2020-05-28 20:47:00 -0400

    Create OpBits.h

---
+++ OpBits.h
@@ -0,0 +1,16 @@
+// OpBits.h
+#ifndef OPBITS_H
+#define OPBITS_H
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+struct _OpBits {
+	char* operation;
+	char* opCode;
+	
+};
+
+typedef struct _OpBits OpBits;
+
+#endif
