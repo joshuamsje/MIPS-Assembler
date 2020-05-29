ec55758692e8c518829fc14dc76e09053824e5ec
joshuamsje <49621706+joshuamsje@users.noreply.github.com>
2020-05-28 20:44:38 -0400

    Create Funct.h

---
+++ Funct.h
@@ -0,0 +1,16 @@
+// Funct.h
+#ifndef FUNCT_H
+#define FUNCT_H
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+struct _Funct{
+	char* rOperation;
+	char* functCode;
+	
+};
+
+typedef struct _Funct Funct;
+
+#endif
